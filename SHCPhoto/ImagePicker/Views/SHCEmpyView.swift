a8ff4ae991688715e5ac4c7218b6f7ea2dbd7994
SHCcc <578013836@qq.com>
2018-07-13 17:29:33 +0800

    增加空白页

---
+++ SHCPhoto/ImagePicker/Views/SHCEmpyView.swift
@@ -0,0 +1,20 @@
+//
+//  SHCEmpyView.swift
+//  Kingfisher
+//
+//  Created by 邵焕超 on 2018/7/13.
+//
+
+import UIKit
+
+class SHCEmpyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
